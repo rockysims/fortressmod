36d5d1adbbd0274b4ed980039c45ffad0b2433e2
Rocky Sims <rocky.sims@gmail.com>
2014-08-06 00:33:59 -0700

    Added notes to self

--- com/newyith/fortressmod/FortressMod.java
+++ com/newyith/fortressmod/FortressMod.java
@@ -24,6 +24,10 @@ import cpw.mods.fml.common.registry.GameRegistry;
 //TODO: work on fortress generator gui. see http://www.minecraftforum.net/forums/mapping-and-modding/mapping-and-modding-tutorials/1571074-1-6-2-advanced-minecraft-forge-modding-tutorial-1
 //TODO: work on FortressGeneratorTileEntity class
 
+//TODO: make only the oldest (in terms of when placed) fortress generator effect wall on/off state
+//TODO: on destroy oldest fortress generator make all other fortress generators touching the wall turn into broken version of fortress generator
+//TODO: add FortressEmergencyKey (8 quarts to craft, emergency key only disables fortress if generator generating it was placed by same person)
+//TODO: think about issue: if you turn on fg to generate fortress next to second fortress, then connect the 2 fortresses, then stop generating, that would update the whole wall or leave behind permanent bedrock
 
 @Mod(modid = ModInfo.MODID, name = ModInfo.NAME, version = ModInfo.VERSION)
 public class FortressMod
