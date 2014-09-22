ea29f9c321367a5813ba1791d07a90a42f5abcec
Rocky Sims <rocky.sims@gmail.com>
2014-09-21 19:56:52 -0700

    Finished adding fortress manual (except Stuck page in manual because /stuck feature isn't written yet).

--- com/newyith/fortressmod/items/ItemFortressManual.java
+++ com/newyith/fortressmod/items/ItemFortressManual.java
@@ -1,63 +1,35 @@
 package com.newyith.fortressmod.items;
 
-import com.newyith.fortressmod.Dbg;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import com.google.common.io.CharStreams;
 import com.newyith.fortressmod.ModInfo;
 
-import net.minecraft.client.gui.GuiScreenBook;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.Items;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemEditableBook;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.ItemWritableBook;
-import net.minecraft.world.World;
 
-//TODO: delete next 2 lines
-//ItemWritableBook
-//ItemEditableBook
-public class ItemFortressManual extends ItemEditableBook {
-	
+public class ItemFortressManual extends ItemWrittenBook {
+
 	public ItemFortressManual() {
+		super();
 		setUnlocalizedName("FortressManual");
 		setTextureName(ModInfo.MODID.toLowerCase() + ":" + "fortress_manual");
+		
+		List<String> pages = new ArrayList<String>();
+		
+		try {
+			pages.clear();
+			pages.addAll(Arrays.asList(
+				CharStreams.toString(new InputStreamReader(getClass().getResourceAsStream("ItemFortressManualContents.txt"), "UTF-8"))
+					.split("===+\n"))
+			);
+		} catch (Exception e) {
+			e.printStackTrace();
+			pages.add("Failed to read contents");
+		}
+		
+		this.setPages(pages);
 	}
-	
-	/*
-	@Override
-    @SideOnly(Side.CLIENT)
-    public void addInformation(ItemStack itemStack, EntityPlayer par2, List list, boolean par4)
-    {
-        String s = "the string s here";
-    	list.add(EnumChatFormatting.GRAY + StatCollector.translateToLocalFormatted("book.byAuthor", new Object[] {s}));
-    }
-	//*/
-	
-	
-	
-	@Override
-	public ItemStack onItemRightClick(ItemStack itemStack, World world, EntityPlayer player) {
-		ItemStack book = new ItemStack(Items.written_book);
-		
-		//this.bookPages = nbttagcompound.getTagList("pages", 8);
-		
-		player.displayGUIBook(book);
-		/*
-		public void displayGUIBook(ItemStack p_71048_1_)
-	    {
-	        Item item = p_71048_1_.getItem();
-
-	        if (item == Items.written_book)
-	        {
-	            this.mc.displayGuiScreen(new GuiScreenBook(this, p_71048_1_, false));
-	        }
-	        else if (item == Items.writable_book)
-	        {
-	            this.mc.displayGuiScreen(new GuiScreenBook(this, p_71048_1_, true));
-	        }
-	    }
-	    */
-		return itemStack;
-    }
-	
 
 }
---
+++ com/newyith/fortressmod/items/ItemWrittenBook.java
@@ -0,0 +1,65 @@
+package com.newyith.fortressmod.items;
+
+import java.util.List;
+
+import com.newyith.fortressmod.Dbg;
+import com.newyith.fortressmod.ModInfo;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.client.gui.GuiScreenBook;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemEditableBook;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemWritableBook;
+import net.minecraft.nbt.NBTBase;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
+import net.minecraft.nbt.NBTTagString;
+import net.minecraft.world.World;
+
+public class ItemWrittenBook extends ItemEditableBook {
+	
+	private List<String> pages;
+
+	public void setPages(List<String> pages) {
+		this.pages = pages;
+	}
+	
+	@Override
+	public ItemStack onItemRightClick(ItemStack itemStack, World world, EntityPlayer player) {
+		ItemStack book = new ItemStack(Items.written_book);
+		
+		//create pages
+		NBTTagList pagesTag = new NBTTagList();
+		for (String pageText : this.pages) {
+			pagesTag.appendTag(new NBTTagString(pageText));
+		}
+		
+		//save pages to book
+		NBTTagCompound compound = new NBTTagCompound();
+		compound.setTag("pages", pagesTag);
+		book.setTagCompound(compound);
+		
+		player.displayGUIBook(book);
+		
+		return itemStack;
+    }
+	
+	@SideOnly(Side.CLIENT)
+    public boolean hasEffect(ItemStack p_77636_1_) {
+        return false;
+    }
+
+	/*
+	@Override
+    @SideOnly(Side.CLIENT)
+    public void addInformation(ItemStack itemStack, EntityPlayer par2, List list, boolean par4)
+    {
+        String s = "the string s here";
+    	list.add(EnumChatFormatting.GRAY + StatCollector.translateToLocalFormatted("book.byAuthor", new Object[] {s}));
+    }
+	//*/
+}
